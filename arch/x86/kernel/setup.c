d0bc65af05c95b9bb4bcf306567e233e5ad4087b
Angelina Zaccaria <angelina.zaccaria@gmail.com>
2020-02-28 15:56:55 -0500

    Hello World

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -885,6 +885,7 @@ void __init setup_arch(char **cmdline_p)
 	__flush_tlb_all();
 #else
 	printk(KERN_INFO "Command line: %s\n", boot_command_line);
+	printk("Hello World from Angelina Zaccaria (10419577)");
 #endif
 
 	/*
